78898cba36fc1f815519e24033615e969541e892
ItsJonQ <itsjonq@gmail.com>
2016-06-27 17:21:04 -0400

    Aww yea. isBlank is working

---
+++ cli.js
@@ -0,0 +1,22 @@
+// Seed CLI :: CLI
+
+const meow = require('meow');
+
+var cli = meow(`
+  Usage:
+    seed -n <name of pack>
+
+  Options:
+    -n, --new Create a new Seed pack
+
+  Example:
+    seed --new=color
+
+    This will generate a new directory for you, in this case, seed-color.
+`, {
+  alias: {
+    n: 'new'
+  }
+});
+
+module.exports = cli;
--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 // Seed CLI
 'use strict';
 
-const _ = require('lodash')
-const authorsToMd = require('authors-to-markdown')
-const chalk = require('chalk')
-const fileExists = require('file-exists')
-const fs = require('fs')
-const isBlank = require('is-blank')
-const isPresnet = require('is-present')
-const meow = require('meow')
-const mkdirp = require('mkdirp')
-const trailingLines = require('single-trailing-newline')
+const requireDir = require('require-dir', { log: false });
+global.cli = require('./cli.js');
 
-
-const cli = meow(`
-  Usage
-    $ seed <name of pack>
-
-  Options
-    -n, --new Create a new Seed pack
-
-  Example
-    $ seed --new=seed-color
-`, {
-  alias: {
-    n: 'new'
-  }
-});
-
-
+// Require all commands
+requireDir('./commands', { recurse: true });
