4f5fee58ff0bf3855c7a1b862b770ce231ba4bf0
ItsJonQ <itsjonq@gmail.com>
2016-06-30 12:37:58 -0400

    Adds module templates + generator

--- commands/generate/index.js
+++ commands/generate/index.js
@@ -6,10 +6,20 @@ var command = cli.input[0];
 var type = cli.input[1];
 var name = cli.input[2];
 
+var generate = require('./generate');
+var help = require('./help');
 
 if (command === 'generate' || command === 'g') {
   if (type && name) {
+    var options = {
+      name: name,
+      type: type
+    };
 
+    return generate(options);
+  }
+  else {
+    console.log(help);
   }
 }
 else {
