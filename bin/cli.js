b1e658568875a693f90d6905b6a58302daf03159
ItsJonQ <itsjonq@gmail.com>
2016-06-30 10:30:13 -0400

    Reorganized cli to /bin/ directory

---
+++ bin/cli.js
@@ -0,0 +1,33 @@
+// Seed CLI :: CLI
+
+var meow = require('meow');
+var pkg = require('../package');
+
+var cli = meow(`
+  Usage:
+    seed <command>
+
+  Commands:
+    new             Creates a new Seed package
+
+  Options:
+    -c, --config    Custom scss-lint config (.yml)
+    -i, --ignore    Ignore files for linting/testing
+    -l, --lint      Initialize .scss lint (powered by sass-lint)
+
+  Aliases:
+    init            alias of new
+
+  ${pkg.name} v${pkg.version}
+  License: ${pkg.license}
+  Website: ${pkg.homepage}
+`, {
+  alias: {
+    c: 'config',
+    i: 'ignore',
+    n: 'new',
+    l: 'lint'
+  }
+});
+
+module.exports = cli;
