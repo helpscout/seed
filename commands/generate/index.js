dd88d30017d2be511038df33eb0f59ac1a496520
ItsJonQ <itsjonq@gmail.com>
2016-06-30 11:04:35 -0400

    Moved templates and improved console feedback

---
+++ commands/generate/index.js
@@ -0,0 +1,17 @@
+// Commands :: Generate :: Index
+'use strict';
+
+var cli = global.cli;
+var command = cli.input[0];
+var type = cli.input[1];
+var name = cli.input[2];
+
+
+if (command === 'generate' || command === 'g') {
+  if (type && name) {
+
+  }
+}
+else {
+  return false;
+}
