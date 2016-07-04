5aaaa4b164ee7260f0e8a9d390519e80011a1948
Seed <itsjonq@gmail.com>
2016-07-04 14:59:08 -0400

    First commit!

---
+++ scripts/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var sass = require('node-sass');
+
+sass.render({
+  file: './scss/pack/_seed-spacing.scss'
+}, function(error, result) {
+  if (error) {
+    console.error(error);
+    return process.exit(1);
+  }
+  else {
+    return console.log('Success!');
+  }
+});
