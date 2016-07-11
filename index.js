2e2c191f72b7c0cbb15d4548fc817b96db512a4e
ItsJonQ <itsjonq@gmail.com>
2016-07-11 08:39:35 -0400

    Adjusts includePaths on index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var path = require('path');
 var seedProps = require('seed-props');
 
-var includePath = [
+var includePath = function() {
+  var paths = Array.prototype.slice.call(arguments);
+  return [].concat.apply([], paths);
+};
+
+var files = [
   seedProps,
   path.join(__dirname, 'scss')
 ];
 
-module.exports = includePath;
+module.exports = includePath(files);
