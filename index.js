2038ecf3d1029caad2bb7f5dccce1c87ad63116b
Seed <itsjonq@gmail.com>
2016-07-13 14:01:42 -0400

    Updates file structure

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var path = require('path');
+var pathfinder = require('./scripts/pathfinder');
 
-var includePath = function() {
-  var paths = Array.prototype.slice.call(arguments);
-  return [].concat.apply([], paths);
-};
-
-var files = [
+var files = pathfinder(
   path.join(__dirname, 'scss')
-];
+);
 
-module.exports = includePath(files);
+module.exports = files;
--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,17 @@
 'use strict';
 
+var pkg = require('../package.json');
 var sass = require('node-sass');
+var pathfinder = require('./pathfinder');
+
+var file = pkg.name;
+var includePaths = pathfinder(
+  // Add files/paths to include
+);
 
 sass.render({
-  file: './scss/pack/_seed-reset.scss'
+  file: './scss/pack/'+file+'/__index.scss',
+  includePaths: includePaths
 }, function(error, result) {
   if (error) {
     console.error(error);
