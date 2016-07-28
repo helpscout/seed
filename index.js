f51948d431a6556ef8877371ce17bb8922f5c634
Seed <itsjonq@gmail.com>
2016-07-28 11:33:55 -0400

    Abstracts pack filtering to seed-packfinder

--- index.js
+++ index.js
@@ -1,84 +1,19 @@
 var _ = require('lodash');
-var fs = require('fs');
-var path = require('path');
 var pathfinder = require('./scripts/pathfinder');
-var findRoot = require('find-root');
-var root = findRoot(process.cwd());
-
-// Define the export array
-var getKeywords = function(pkg) {
-  var _path = path.join(root, 'node_modules', pkg, 'package.json');
-  var stat = fs.statSync(_path).isFile();
-
-  if (!stat) {
-    console.log(pkg + ' could not be found in node_modules.');
-    console.log('Running npm install might help!');
-    return process.exit(1);
-  }
-  else {
-    var module = require(_path);
-    var keywords = module.keywords;
-    if (keywords) {
-      return keywords;
-    }
-    else {
-      return false;
-    }
-  }
-};
-
-var isSeedPack = function(pkg) {
-  var keywords = getKeywords(pkg);
-  if (keywords) {
-    return _.includes(keywords, 'seed-pack');
-  }
-  else {
-    return false;
-  }
-};
-
-var getDependencies = function() {
-  var pkg = require(path.join(root, 'package.json'));
-  var devDeps = pkg.devDependencies;
-  var deps = pkg.dependencies;
-  var dependencies = [];
-
-  if (devDeps) {
-    _.forEach(_.keys(devDeps), function(pkg) {
-      dependencies.push(pkg);
-    });
-  }
-
-  if (deps) {
-    _.forEach(_.keys(deps), function(pkg) {
-      dependencies.push(pkg);
-    });
-  }
-
-  if (dependencies.length) {
-    return dependencies;
-  }
-  else {
-    return false;
-  }
-};
-
+var packfinder = require('seed-packfinder');
 
 var harvester = function() {
   // Define the export array
   var includePaths = [];
   var paths = pathfinder(arguments);
-  var dependencies = getDependencies();
+  var packs = packfinder.find();
 
-  if (!dependencies) {
+  if (!packs) {
     return false;
   }
 
-  _.forEach(dependencies, function(pkg) {
-    if (isSeedPack(pkg)) {
-      // Push to includePaths if is seed-pack
-      includePaths.push(require(pkg));
-    }
+  _.forEach(packs, function(pack) {
+    includePaths.push(require(pack));
   });
 
   if (paths.length) {
