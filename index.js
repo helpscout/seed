c0829548bccbdb4edca411a2463e13866320533a
Seed <itsjonq@gmail.com>
2016-07-29 15:46:03 -0400

    Fixes pathfinder path in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var pathfinder = require('sass-pathfinder./scripts/pathfinder');
+var pathfinder = require('sass-pathfinder');
 
 var files = pathfinder(
   require('seed-color-scheme'),
