5dc03c6560068107b2d4c4d4efd54da6a8809721
Seed <itsjonq@gmail.com>
2016-10-29 10:32:39 -0400

    Implements seed-publish

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var pathfinder = require('./scripts/pathfinder');
 var files = pathfinder(
   require('seed-dash'),
   require('seed-props'),
+  require('seed-publish'),
   path.join(__dirname, 'scss')
 );
 
--- scripts/build.js
+++ scripts/build.js
@@ -9,7 +9,8 @@ var sass = require('node-sass');
 var file = pkg.name;
 var includePaths = pathfinder(
   require('seed-dash'),
-  require('seed-props')
+  require('seed-props'),
+  require('seed-publish')
 );
 
 // Default .css compile
