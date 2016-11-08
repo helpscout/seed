adf2347456a135ad1867b90d51f6d78edd825eb2
Seed <itsjonq@gmail.com>
2016-11-08 15:55:52 -0500

    Scopes link reset styles to non-buttons

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var pathfinder = require('sass-pathfinder');
 
 var files = pathfinder(
   require('seed-badge'),
+  require('seed-button'),
   require('seed-dash'),
   require('seed-publish'),
   require('seed-states'),
--- scripts/build.js
+++ scripts/build.js
@@ -9,6 +9,7 @@ var sass = require('node-sass');
 var file = pkg.name;
 var includePaths = pathfinder(
   require('seed-badge'),
+  require('seed-button'),
   require('seed-dash'),
   require('seed-publish'),
   require('seed-states')
