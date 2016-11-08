9a67f0cf8f1766d94bd954f4b493ebe93effbaf8
Seed <itsjonq@gmail.com>
2016-11-08 14:37:03 -0500

    Adds badge styles and normalizes links

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path');
 var pathfinder = require('sass-pathfinder');
 
 var files = pathfinder(
+  require('seed-badge'),
   require('seed-dash'),
   require('seed-publish'),
   require('seed-states'),
