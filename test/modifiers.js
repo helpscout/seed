5cfc060fd8f44f243f4224e88561209509bc937d
ItsJonQ <itsjonq@gmail.com>
2017-03-18 15:49:25 -0400

    Implements seed-barista test helper

--- test/modifiers.js
+++ test/modifiers.js
@@ -1,23 +1,24 @@
 // Test :: Input
-var assert = require('assert');
-// Helper
-var sass = require('./helper/sass');
+var assert = require('chai').assert;
+var barista = require('seed-barista');
 
 describe('seed-input: modifiers', function() {
   describe('sizes', function() {
-    var output = sass('_input-sizes.scss');
+    var output = barista({
+      file: '_input-sizes.scss',
+    });
 
     it('should generate custom sizes when $seed-control-sizes is modified', function() {
       // Class structure should be
       // html select.c-input { ... }
-      var expects = output.indexOf('.c-input--size-sm') >= 0;
+      var expects = output.css.indexOf('.c-input--size-sm') >= 0;
       assert.equal(expects, true);
     });
 
     it('should omit default "lg" sizes when $seed-control-sizes is modified', function() {
       // Class structure should be
       // html select.c-input { ... }
-      var expects = output.indexOf('.c-input--lg') < 0;
+      var expects = output.css.indexOf('.c-input--lg') < 0;
       assert.equal(expects, true);
     });
   });
