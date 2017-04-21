4d002c1c5095f7d3b9dbe8e5b5795ee007b0ae6a
Seed <itsjonq@gmail.com>
2017-04-21 12:37:19 -0400

    Adds seed-publish test

--- test/_build.js
+++ test/_build.js
@@ -14,4 +14,23 @@ describe('seed-dot-nav: build', function() {
   it('should build', function() {
     expect(output.css).to.exist;
   });
+
+  it('should build only once', function() {
+    var style = `
+      @import "./_index";
+      @import "./_index";
+      @import "./_index";
+      @import "./_index";
+      @import "./_index";
+      @import "./_index";
+    `;
+    var output = barista({ content: style });
+    var $c = output.$('.c-dot-nav');
+    var $i = output.$('.c-dot-nav__item');
+    var $l = output.$('.c-dot-nav__link');
+
+    expect($c.selectors.length).to.equal(1);
+    expect($i.selectors.length).to.equal(1);
+    expect($l.selectors.length).to.equal(1);
+  });
 });
