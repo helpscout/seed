159749e3304566a75a5022bdbd15fdb81ab574cb
ItsJonQ <itsjonq@gmail.com>
2017-05-26 11:49:39 -0400

    Update dependencies + add tests

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var path = require('path');
 var pathfinder = require('./scripts/pathfinder');
 
 var files = pathfinder(
-  require('seed-publish'),
-  path.join(__dirname, 'scss')
+  require('seed-publish')
+//   path.join(__dirname, 'scss')
 );
 
 module.exports = files;
+console.log(pathfinder)
