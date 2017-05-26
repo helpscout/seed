53b985f5727ea391b373b7014344e1c2874d32b5
ItsJonQ <itsjonq@gmail.com>
2017-05-26 17:33:56 -0400

    Update dependencies and add tests

---
+++ test/mixin.between.js
@@ -0,0 +1,44 @@
+// Test :: Mixin :: Between
+/* globals describe: true, it: true */
+'use strict';
+
+var barista = require('seed-barista');
+var expect = require('chai').expect;
+
+describe('seed-breakpoints: mixin: between', function() {
+  it('should accept breakpoint sizes (defined in _config.scss)', function() {
+    var style = `
+      @import "./_index";
+      .machi {
+        @include breakpoint-between(sm, lg) {
+          position: absolute;
+        }
+      }
+    `;
+    var output = barista({ content: style });
+    var rule = output.rule('.machi');
+    var mq = rule.mq();
+
+    expect(rule.exists()).to.be.true;
+    expect(mq.rule).to.include('min-width: 544');
+    expect(mq.rule).to.include('max-width: 991');
+  });
+
+  it('should accept numerical sizes (e.g. px)', function() {
+    var style = `
+      @import "./_index";
+      .machi {
+        @include breakpoint-between(400px, 80em) {
+          position: absolute;
+        }
+      }
+    `;
+    var output = barista({ content: style });
+    var rule = output.rule('.machi');
+    var mq = rule.mq();
+
+    expect(rule.exists()).to.be.true;
+    expect(mq.rule).to.include('min-width: 400px');
+    expect(mq.rule).to.include('max-width: 80em');
+  });
+});
