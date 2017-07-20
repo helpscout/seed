ea70509fbcf22cc1e2a668cb67a1e584fff0fba1
ItsJonQ <itsjonq@gmail.com>
2017-07-20 14:58:49 -0400

    Link: Remove box-shadow on click (active)
    * Normalize box-shadow for link modifier (active, focus, focus:active)
    * Add tests

--- test/modifiers.link.js
+++ test/modifiers.link.js
@@ -13,6 +13,12 @@ describe('seed-button: modifiers: link', () => {
       expect(o.prop('border-color')).to.equal('transparent');
     });
 
+    it('should not have box-shadow when clicked', () => {
+      expect(output.rule('.c-button--link:active').prop('box-shadow')).to.equal('none');
+      expect(output.rule('.c-button--link:focus').prop('box-shadow')).to.equal('none');
+      expect(output.rule('.c-button--link:focus:active').prop('box-shadow')).to.equal('none');
+    });
+
     it('should not have visible borders when focused + state', () => {
       expect(output.rule('.c-button--link.is-error:focus').prop('border-color')).to.equal('transparent');
       expect(output.rule('.c-button--link.is-success:focus').prop('border-color')).to.equal('transparent');
