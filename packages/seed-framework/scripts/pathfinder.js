11af9e590e70f7ab3be3162f900b396e9385b9df
ItsJonQ <itsjonq@gmail.com>
2018-07-04 15:36:44 -0400

    Install hoek for packages

--- packages/seed-framework/scripts/pathfinder.js
+++
@@ -1,9 +0,0 @@
-// Sass Pathfinder
-// Source: https://github.com/ItsJonQ/sass-pathfinder
-
-var pathfinder = function() {
-  var paths = Array.prototype.slice.call(arguments);
-  return [].concat.apply([], paths);
-};
-
-module.exports = pathfinder;
