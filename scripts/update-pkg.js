f0e2bc35cee6d4fac9177580bd715bbfb7dfe974
Jon Q <itsjonq@gmail.com>
2018-09-28 17:21:11 -0400

    Update packages to use postversion

--- scripts/update-pkg.js
+++ scripts/update-pkg.js
@@ -11,7 +11,7 @@ glob("packages/*/package.json", (err, files) => {
       publishConfig: {
         access: "public"
       },
-      prepublish: "npm run build",
+      postversion: "npm run build",
       homepage: "https://github.com/helpscout/seed/",
       repository: {
         type: "git",
